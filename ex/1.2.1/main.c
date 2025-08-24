8918d91efcd7540d33ef7bbdd0a899645ecd06f0
theo-al <emailtheoprauerj@gmail.com>
2025-08-24 01:11:16 -0300

    1.2.1: alfabeto estendido com letras mais difíceis
    adicionados retângulos menores para fazer as letras novas
    pangramas colocados na tela

--- ex/1.2.1/main.c
+++ ex/1.2.1/main.c
@@ -3,9 +3,9 @@
 #include "cores.h"
 
 
+/* DEFINIÇÕES */
 #define LEN(arr) (sizeof(arr)/sizeof(*arr))
 
-/* DEFINIÇÕES */
 #define WINDOW_WIDTH  720
 #define WINDOW_HEIGHT 720
 
@@ -37,51 +37,94 @@ void desenho_recursivo(SDL_Renderer* ren, const uint16_t escala) {
 void desenhar_texto(SDL_Renderer* ren,
                     const char* texto, const int tam_fonte, 
                     const int x, const int y) {
-    #define HORI sz, ln
-    #define VERT ln, sz
-    
     const int sz  = tam_fonte;
     const int ln  = tam_fonte/8;
     const int pad = tam_fonte/4;
 
     const int end = sz - ln;
+    const int mid = end/2;
 
-    const SDL_Rect TOP_H = { 0,     0, HORI };
-    const SDL_Rect BOT_H = { 0,   end, HORI };
-    const SDL_Rect MID_H = { 0, end/2, HORI };
+    #define HORI sz, ln
+    #define VERT ln, sz
+      const SDL_Rect TOP_H = { 0,   0, HORI };
+      const SDL_Rect BOT_H = { 0, end, HORI };
+      const SDL_Rect MID_H = { 0, mid, HORI };
 
-    const SDL_Rect ESQ_V = { 0,     0, VERT };
-    const SDL_Rect DIR_V = { end,   0, VERT };
-    const SDL_Rect MID_V = { end/2, 0, VERT };
+      const SDL_Rect ESQ_V = {   0, 0, VERT };
+      const SDL_Rect DIR_V = { end, 0, VERT };
+      const SDL_Rect MID_V = { mid, 0, VERT };
+    #undef VERT
+    #undef HORI
+
+    #define MEIO_HORI sz/2, ln
+    #define MEIO_VERT ln, sz/2
+      const SDL_Rect MEIO_H_MID_ESQ = {   0, mid, MEIO_HORI };
+      const SDL_Rect MEIO_H_MID_DIR = { mid, mid, MEIO_HORI };
+
+      const SDL_Rect MEIO_H_TOP_ESQ = {   0, 0, MEIO_HORI };
+      const SDL_Rect MEIO_H_TOP_DIR = { mid, 0, MEIO_HORI };
+
+      const SDL_Rect MEIO_H_BOT_ESQ = {   0, end, MEIO_HORI };
+      const SDL_Rect MEIO_H_BOT_DIR = { mid, end, MEIO_HORI };
+
+      const SDL_Rect MEIO_V_TOP_ESQ = {   0, 0, MEIO_VERT };
+      const SDL_Rect MEIO_V_TOP_MID = { mid, 0, MEIO_VERT };
+      const SDL_Rect MEIO_V_TOP_DIR = { end, 0, MEIO_VERT };
+
+      //const SDL_Rect MEIO_V_BOT_ESQ = {   0, end, MEIO_VERT };
+      const SDL_Rect MEIO_V_BOT_MID = { mid, mid, MEIO_VERT };
+      const SDL_Rect MEIO_V_BOT_DIR = { end, mid, MEIO_VERT };
+    #undef MEIO_VERT
+    #undef MEIO_HORI
 
     const struct SDL_Rect FONTE[][6] = {
-        ['-'] = {        MID_H,                              },
         ['a'] = { TOP_H, MID_H,         ESQ_V,        DIR_V, },
-        ['b'] = { TOP_H, MID_H, BOT_H,  ESQ_V,        DIR_V, },
+
         ['c'] = { TOP_H,        BOT_H,  ESQ_V,               },
-        ['d'] = { TOP_H,        BOT_H,  ESQ_V,        DIR_V, }, //! o
+        ['d'] = { TOP_H,        BOT_H,         MID_V, DIR_V, },
         ['e'] = { TOP_H, MID_H, BOT_H,  ESQ_V,               },
         ['f'] = { TOP_H, MID_H,         ESQ_V,               },
-        //! falta g
+
         ['h'] = {        MID_H,         ESQ_V,        DIR_V, },
-        ['i'] = {                       MID_V,               },
-        ['j'] = { BOT_H,                DIR_V,               },
-        //! falta k
-        ['l'] = { BOT_H,                ESQ_V,               },
-        ['n'] = { TOP_H,                ESQ_V,        DIR_V, }, //! m
-        ['m'] = { TOP_H,                ESQ_V,        DIR_V, }, //! n
+        ['i'] = { TOP_H,        BOT_H,         MID_V,        },
+        ['j'] = {               BOT_H,  DIR_V,               },
+
+        ['l'] = {               BOT_H,  ESQ_V,               },
+        ['m'] = { TOP_H,                ESQ_V, MID_V, DIR_V, },
+        ['n'] = { TOP_H,                ESQ_V,        DIR_V, },
         ['o'] = { TOP_H,        BOT_H,  ESQ_V,        DIR_V, },
-        //! falta p
-        //! falta q
-        //! falta r
-        //! falta s
+
         ['t'] = { TOP_H,                       MID_V,        },
-        ['u'] = { BOT_H,                ESQ_V,        DIR_V, },
-        ['v'] = { BOT_H,                ESQ_V,        DIR_V, }, //! u
-        //! falta w
-        //! falta x
-        //! falta y
-        //! falta z
+        ['u'] = {               BOT_H,  ESQ_V,        DIR_V, },
+
+        ['w'] = {               BOT_H,  ESQ_V, MID_V, DIR_V, },
+        ['x'] = {        MID_H,                MID_V,        }, //!
+
+        ['b'] = {
+            MID_H, BOT_H,  ESQ_V,
+            MEIO_V_BOT_DIR,
+        },
+        ['g'] = {
+            TOP_H, BOT_H,  ESQ_V,
+            MEIO_H_MID_DIR, MEIO_V_BOT_DIR,
+        },
+        ['k'] = {
+            ESQ_V,
+            MEIO_H_BOT_DIR, MEIO_H_TOP_DIR,
+            MEIO_H_MID_ESQ,
+            MEIO_V_TOP_MID, MEIO_V_BOT_MID,
+        },
+        ['p'] = { TOP_H, MID_H,  ESQ_V,         MEIO_V_TOP_DIR, },
+        ['q'] = { TOP_H, BOT_H,  ESQ_V, DIR_V,  MEIO_V_BOT_MID, },
+        ['r'] = { MID_V,  MEIO_H_TOP_DIR, },
+        ['s'] = { MID_V,  MEIO_H_BOT_ESQ, MEIO_H_TOP_DIR, },
+        ['v'] = {
+            ESQ_V,
+            MEIO_V_BOT_MID, MEIO_V_TOP_DIR,
+            MEIO_H_MID_DIR, MEIO_H_BOT_ESQ,
+        },
+        ['y'] = { MID_H, BOT_H, DIR_V,  MEIO_V_TOP_ESQ,   },
+        ['z'] = { MID_V,  MEIO_H_BOT_DIR, MEIO_H_TOP_ESQ, },
     };
 
     for (size_t i = 0; texto[i]; i++) {
@@ -89,9 +132,6 @@ void desenhar_texto(SDL_Renderer* ren,
         desenhar_lista_offs(ren, FONTE[c], LEN(FONTE[c]),
                                  x+(tam_fonte+pad)*i, y);
     }
-
-    #undef VERT
-    #undef HORI
 }
 
 int main() {
@@ -112,15 +152,18 @@ int main() {
     const int escala = WINDOW_WIDTH*2/3;
     desenho_recursivo(ren, escala);
 
+
     mudar_cor(ren, CINZA);
     desenhar_texto(ren, "theo", 40, WINDOW_WIDTH*2/3, 40);
-    //desenhar_texto(ren, "the lazy brown fox jumped over the lazy dog", 40,
-    //               WINDOW_WIDTH*2/3, WINDOW_WIDTH - 40);
-    //desenhar_texto(ren, "TV faz quengo explodir com whisky JB", 40,
-    //               WINDOW_WIDTH*2/3, WINDOW_WIDTH - 40);
+
+    const int tam_fonte = 12, pad = tam_fonte/4;
+    desenhar_texto(ren, "tv faz quengo explodir com whisky jb",
+                   tam_fonte, pad, WINDOW_WIDTH - pad - (tam_fonte+pad)*2);
+    desenhar_texto(ren, "the quick brown fox jumped over the lazy dog",
+                   tam_fonte, pad, WINDOW_WIDTH - pad - (tam_fonte+pad)*3);
 
     SDL_RenderPresent(ren);
-    SDL_Delay(6000); 
+    SDL_Delay(10000); 
 
     /* FINALIZAÇÃO */
     SDL_DestroyRenderer(ren);
