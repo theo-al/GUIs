955b04ccb5bcc560a31412ad1530c89562bf257c
theo-al <emailtheoprauerj@gmail.com>
2025-09-07 21:27:31 -0300

    1.5.1: menos feio, lidando melhor com o teclado

--- ex/1.5.1/main.c
+++ ex/1.5.1/main.c
@@ -12,6 +12,16 @@
 #define RW (HEIGHT/10)
 #define RH RW
 
+#define VEL (10.0/1000)
+
+int32_t dt(uint32_t antes, uint32_t* depois) {
+    uint32_t agora = SDL_GetTicks();
+    uint32_t delta = agora - antes;
+
+    if (depois) *depois = agora;
+    return delta;
+}
+
 static const SDL_Rect janela = {.w = WIDTH, .h = HEIGHT};
 int main () {
     /* INICIALIZAÇÃO DO SDL */
@@ -23,9 +33,8 @@ int main () {
                       );
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
-    /* INICIALIZAÇÃO DO ESTADO */
-    uint32_t antes = SDL_GetTicks();
 
+    /* INICIALIZAÇÃO DOS RETÂNGULOS */
     SDL_Color cores[3] = { AZUL, VERDE, AMARELO, };
     SDL_FRect rects[3] = {
         { 40,20, RW,RH }, { 60,50, RW,RH }, { 10,20, RW,RH },
@@ -36,41 +45,46 @@ int main () {
               *teclado = &rects[r++],
               *mouse   = &rects[r++];
 
-    /* EXECUÇÃO */
-    for (SDL_Event evt; evt.type != SDL_QUIT; ) {
-        /* EVENTOS E TEMPORIZAÇÃO */
-        SDL_WaitEventTimeout(&evt, TIMEOUT);
+    SDL_FRect vel_teclado = {0}; //! tipo errado
 
-        uint32_t agora = SDL_GetTicks();
-        uint32_t delta = agora - antes;
+
+    /* EXECUÇÃO */
+    uint32_t antes = SDL_GetTicks();
+    for (SDL_Event evt; evt.type != SDL_QUIT; ) {
+        SDL_WaitEventTimeout(&evt, TIMEOUT);
+        uint32_t delta = dt(antes, &antes);
 
         switch (evt.type) {
-          /* RETÂNGULO MOVIDO POR MOUSE */
+          case SDL_KEYDOWN: {
+              switch (evt.key.keysym.sym) {
+                case SDLK_UP:    vel_teclado.y = -VEL; break;
+                case SDLK_DOWN:  vel_teclado.y = +VEL; break;
+                case SDLK_LEFT:  vel_teclado.x = -VEL; break;
+                case SDLK_RIGHT: vel_teclado.x = +VEL; break;
+              }
+          } break;
+          case SDL_KEYUP: {
+              switch (evt.key.keysym.sym) {
+                case SDLK_UP:   case SDLK_DOWN:  vel_teclado.y = 0; break;
+                case SDLK_LEFT: case SDLK_RIGHT: vel_teclado.x = 0; break;
+              }
+          } break;
+
           case SDL_MOUSEMOTION: {
               mouse->x = evt.button.x -RW/2;
               mouse->y = evt.button.y -RH/2;
-          } break;
-
-          /* RETÂNGULO MOVIDO POR TECLADO */
-          case SDL_KEYDOWN: {
-              switch (evt.key.keysym.sym) {
-                case SDLK_UP:    teclado->y -= 5; break;
-                case SDLK_DOWN:  teclado->y += 5; break;
-                case SDLK_LEFT:  teclado->x -= 5; break;
-                case SDLK_RIGHT: teclado->x += 5; break;
-              }
               TFX_ClampRectPosF(teclado, janela);
           } break;
         }
 
-        /* RETÂNGULO MOVIDO POR TEMPO */ {
-            const float vel = 10;
-            tempo->x += (vel/1000)*delta;
-            tempo->y += (vel/1000)*delta;
-            TFX_WrapRectPosF(tempo, janela);
-        }
+        /* MOVIMENTO DOS RETÂNGULOS */
+        teclado->x += vel_teclado.x*delta;
+        teclado->y += vel_teclado.y*delta;
+        TFX_ClampRectPosF(teclado, janela);
 
-        antes = agora;
+        tempo->x += VEL*delta;
+        tempo->y += VEL*delta;
+        TFX_WrapRectPosF(tempo, janela);
 
         /* RENDERIZAÇÃO */
         TFX_limpar_tela_cor(ren, BRANCO);
@@ -78,7 +92,7 @@ int main () {
         SDL_RenderPresent(ren);
     }
 
-    /* FINALIZACAO */
+    /* FINALIZAÇÃO */
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
     SDL_Quit();
