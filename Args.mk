67350cfbc8f6f659c962bdbb0056207174b7802c
theo-al <emailtheoprauerj@gmail.com>
2025-09-09 10:34:18 -0300

    makefile: comando run

--- Args.mk
+++ Args.mk
@@ -8,9 +8,27 @@ $(EXD):; @:
 $(ARQ):; @:
 do:
 	@if [ -z "$(EXD)" ] || [ -z "$(ARQ)" ]; then \
-	    echo "Uso: make do <ex> <ar>"; exit 1; \
+	    echo "Uso: make $@ <ex> <ar>"; exit 1; \
 	fi
 	@$(MAKE) ex/$(EXD)/$(ARQ).out ex/$(EXD)/$(ARQ).gif
 
 .PHONY: do
+endif
+
+ifeq ($(CMD),run)
+EXD := $(word 2, $(MAKECMDGOALS))
+ARQ := $(word 3, $(MAKECMDGOALS))
+
+$(EXD):; @:
+$(ARQ):; @:
+run:
+	@if [ -z "$(EXD)" ]; then \
+	    echo "Uso: make $@ <ex> [ar]"; exit 1; \
+	fi;
+	@
+	@if [ -z "$(ARQ)" ]; then ARQ=main; fi; \
+	$(MAKE) ex/$(EXD)/"$$ARQ".out; \
+	ex/$(EXD)/"$$ARQ".out;
+
+.PHONY: run
 endif
