2b268575ae448a33cd0fd0b24ebffa02864dd946
theo-al <emailtheoprauerj@gmail.com>
2025-09-29 03:49:44 -0300

    2.2: vários quadrados empilháveis

--- ex/2.2/main.c
+++ ex/2.2/main.c
@@ -22,6 +22,12 @@
 #define ATRITO 0.5
 
 
+static inline
+void AUX_ToEndSzLen(void* arr, size_t size, size_t len, size_t idx);
+#define AUX_ToEndLen(arr, len, i) AUX_ToEndSzLen(arr, sizeof(*arr), len, i)
+#define AUX_ToEnd(arr, i) AUX_ToEndLen(arr, LEN(arr), i)
+
+
 typedef enum {
     AUX_TIMEOUTEVENT = 0,
 } AUX_EventType;
@@ -38,6 +44,7 @@ void AUX_NextEvent(SDL_Event* evt, uint32_t* falta, uint32_t timeout) {
     if (AUX_WaitEventTimeout(evt, falta, timeout));
     else AUX_FillTimeout(evt);
 }
+
 
 typedef struct {
     SDL_Rect r;
@@ -87,7 +94,7 @@ void AUX_DragDropCancel(DragDropRect* self, SDL_Event evt) {
 int main() {
     /* INICIALIZACAO */
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* win = SDL_CreateWindow("Drag&Drop",
+    SDL_Window* win = SDL_CreateWindow("Drag, Drop, Cancel",
                          SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED,
                          W_WIDTH, W_HEIGHT, SDL_WINDOW_SHOWN
@@ -97,33 +104,61 @@ int main() {
     /* EXECUÇÃO */
     uint32_t falta = TIMEOUT;
     for (SDL_Event evt; evt.type != SDL_QUIT; ) {
-        static DragDropRect quadrado = {
-            .r.x = W_WIDTH/2,  .r.w = W_WIDTH/10,
-            .r.y = W_HEIGHT/2, .r.h = W_WIDTH/10,
+        const int rw = W_WIDTH/10, hmid = (W_HEIGHT-rw)/2;
+        static DragDropRect quadrados[] = {
+          { .r.x = W_WIDTH*1/3-rw/2, .r.y = hmid,    .r.w=rw, .r.h=rw },
+          { .r.x = W_WIDTH*2/3-rw/2, .r.y = hmid,    .r.w=rw, .r.h=rw },
+          { .r.x = W_WIDTH/2  -rw/2, .r.y = hmid-rw, .r.w=rw, .r.h=rw },
+          { .r.x = W_WIDTH/2  -rw/2, .r.y = hmid+rw, .r.w=rw, .r.h=rw },
         };
 
         AUX_NextEvent(&evt, &falta, TIMEOUT);
-        AUX_DragDropCancel(&quadrado, evt);
         switch (evt.type) {
           case SDL_USEREVENT: switch (evt.user.code) {
               case AUX_TIMEOUTEVENT: {
                   TFX_limpar_tela_cor(ren, BRANCO);
 
-                  SDL_Color cor[] = { PRETO, AZUL, VERMELHO };
-                  const uint8_t idx = (quadrado.clicking<<0) +
-                                      (quadrado.dragging<<1);
-                  assert(idx < LEN(cor));
-                  TFX_desenhar_rect_cor(ren, quadrado.r, cor[idx]);
+                  for (size_t i = 0; i < LEN(quadrados); i++) {
+                      SDL_Color cor[] = { PRETO, AZUL, VERMELHO };
+                      const uint8_t idx = (quadrados[i].clicking<<0) +
+                                          (quadrados[i].dragging<<1);
+                      assert(idx < LEN(cor));
+                      TFX_desenhar_rect_cor(ren, quadrados[i].r, cor[idx]);
+                  }
 
                   SDL_RenderPresent(ren);
               } break;
           } break;
         }
+
         /* LÓGICA */
+        for (size_t i = LEN(quadrados); i--; ) {
+            AUX_DragDropCancel(&quadrados[i], evt);
+            if (quadrados[i].clicking || quadrados[i].dragging) {
+                AUX_ToEnd(quadrados, i); break;
+            }
+        }
     }
 
     /* FINALIZACAO */
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
     SDL_Quit();
+}
+
+static inline
+void AUX_ToEndSzLen(void* arr, size_t size, size_t len, size_t idx) {
+  #define elem(arr, size, idx) ((arr) + (size)*(idx))
+    char *const base = arr;
+    char *const curr = elem(base, size, idx);
+
+    char buf[size];
+    memcpy(buf, curr, size);
+
+    char *const next = elem(base, size, idx+1);
+    char *const last = elem(base, size, len-1);
+
+    memmove(curr, next, last-curr);
+    memcpy(last, buf, size);
+  #undef elem
 }
